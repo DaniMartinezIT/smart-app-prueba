eab9810050d2892254f465e19f262bc2d2e023cc
esjcast <41775751+esjcast@users.noreply.github.com>
2018-08-09 13:41:24 +0200

    a

--- src/js/grafica.js
+++ src/js/grafica.js
@@ -77,11 +77,11 @@ var finEjeX;
 
     headerHTML.push("<div class='media'>");
     if (pt.gender === "male")
-      headerHTML.push("<img class='align-selft-start mr-3' src='./images/mpatient.png' alt='logo male patient'>");
+      headerHTML.push("<img class='align-selft-start mr-3' src='./src/images/mpatient.png' alt='logo male patient'>");
     else if (pt.gender === "female") 
-      headerHTML.push("<img class='align-selft-start mr-3' src='./images/fpatient.png' alt='logo female patient'>");
+      headerHTML.push("<img class='align-selft-start mr-3' src='./src/images/fpatient.png' alt='logo female patient'>");
     headerHTML.push("<div class='media-body'><h5 class='mt-0'>Datos demográficos</h5>");
-    headerHTML.push("<div class='container-fluid'><div class='row flex-nowrap'><div class='col-md'><b>Nombre y apellidos:</b> "+getPatientName(pt)+"</div></div>");
+    headerHTML.push("<div class='container-fluid'><div class='row flex-nowrap'><div class='col-md-3'><b>Nombre y apellidos:</b> "+getPatientName(pt)+"</div>");
     headerHTML.push(getPatientDetail(pt));
     headerHTML.push("</div></div>");
     $("#patient_name").append(headerHTML.join(" "));
@@ -102,7 +102,8 @@ var finEjeX;
   function getPatientDetail(pt) {
     if (pt.name) {
       var details = pt.name.map(function (name) {
-        return '<div class="row flex-nowrap"><div class="col-sm-3"><b>ID:</b>' + pt.id + '</div>'+
+        return '<div class="col-md-3"><b>MRN: </b>' + pt.MRN + '</div></div>'+
+        '<div class="row flex-nowrap"><div class="col-sm-3"><b>ID:</b>' + pt.id + '</div>'+
         '<div class="col-sm-3"><b>Género:</b>' + pt.gender + '</div>'+
         '<div class="col-sm-6"><b>Fecha de nacimiento:</b>' + pt.birthDate+ '</div></div></div>';
       });
